2d33604ff6b0cf64decac369406fec094762e387
ADITYA KUMAR SINGH <74896018+ADITYAKUMARSINGH16@users.noreply.github.com>
2023-08-31 13:38:54 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "ADITYA KUMAR SINGH",
+  title: "JUST LEARNING HOW TO USE CODESPACE",
+  email: "singhaditya7864@gmail.com",
+  gitHub: "ADITYASINGH16",
+  instagram: "adityasingh21916",
+  linkedIn: "Aditya Kumar Singh",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
